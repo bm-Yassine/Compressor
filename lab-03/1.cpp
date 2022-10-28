187e31ce543a7517a6e920bfd6b9c8bc213130b5
bm-Yassine <83090064+bm-Yassine@users.noreply.github.com>
2022-10-28 14:46:38 +0200

    code

---
+++ lab-03/1.cpp
@@ -0,0 +1,25 @@
+// Ex1
+
+#include <iostream>
+ int main() { 
+    std::cout << std::endl << "for" << std::endl;
+    cin >> "N";
+    for ( int i = 0 ; i < N ; i++)
+    {
+        std::cout << i << "*";
+    }
+ }
+
+// Ex2 
+
+#include <iostream>
+ int main() {
+    std::cout << std::endl << "for" << std::endl;
+    cin >> "N";
+    int n = (N / 2)
+    for ( int i = 0 ; i < n ; i++)
+    {
+        std::cout << i << "*.";
+    }
+
+ }
