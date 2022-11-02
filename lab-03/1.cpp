a6700a208ab6a55d0cadfcb1a5b17a9d6ef87a9c
lasotek <lasotek@gmail.com>
2022-11-02 15:18:21 +0100

    From teacher

--- lab-03/1.cpp
+++ lab-03/1.cpp
@@ -1,25 +1,54 @@
 // Ex1
+// Awful code that does not compile. You can use one file, but it should be compiled
+// int main()
+// {
+// #include <iostream> //??????????????????????????
+//     std::cout << std::endl
+//               << "for" << std::endl;
+//     cin >> "N";
+//     for (int i = 0; i < N; i++)
+//     {
+//         std::cout << i << "*";
+//     }
+// }
+
+// // Ex2
+
+// #include <iostream>
+// int main()
+// {
+//     std::cout << std::endl
+//               << "for" << std::endl;
+//     cin >> "N";
+//     int n = (N / 2) for (int i = 0; i < n; i++)
+//     {
+//         std::cout << i << "*.";
+//     }
+// }
 
 #include <iostream>
- int main() { 
-    std::cout << std::endl << "for" << std::endl;
-    cin >> "N";
-    for ( int i = 0 ; i < N ; i++)
+
+int main()//Anyway in some strange language, but no C++
+{
+    // Ex1
     {
-        std::cout << i << "*";
+        std::cout << std::endl
+                  << "for" << std::endl;
+        cin >> "N";
+        for (int i = 0; i < N; i++)
+        {
+            std::cout << i << "*";
+        }
     }
- }
-
-// Ex2 
-
-#include <iostream>
- int main() {
-    std::cout << std::endl << "for" << std::endl;
-    cin >> "N";
-    int n = (N / 2)
-    for ( int i = 0 ; i < n ; i++)
+    // Ex2
     {
-        std::cout << i << "*.";
+        std::cout << std::endl
+                  << "for" << std::endl;
+        cin >> "N";
+        int n = (N / 2) for (int i = 0; i < n; i++)
+        {
+            std::cout << i << "*.";
+        }
     }
-
- }
+    return 0;
+}
