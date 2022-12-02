bae384ac4c86fcc3945311c5ca1696a1d6b2cbce
bm-Yassine <83090064+bm-Yassine@users.noreply.github.com>
2022-12-02 14:44:00 +0100

    not done yet

---
+++ lab-08/commands.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+#include <string>
+
+  int random_numbers(int N, int min, int max, int inL, std::string f);
+
+#endif
---
+++ lab-08/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include "commands.h"
+
+int main() {
+    int N, min, max, inL;
+    std::string f;
+    std::cout<<"Please input in this order : <number of numbers> <min> <max> <no in line> [<output file name>]\n";
+    std::cin>>N;
+    std::cin>>min;
+    std::cin>>max;
+    std::cin>>inL;
+    std::cin>>f;
+    //random_numbers(N, min, max, inL, f);
+
+
+
+
+    /*
+    srand(time(NULL));
+    int value;
+    std::ofstream file ("numbers.txt");
+    int i = 0;
+    while (i < 1000000)
+    {
+        value = rand(); 
+        file << value;
+    }
+    file.close();
+    */
+
+}
+
